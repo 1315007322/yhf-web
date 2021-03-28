297674deeb4bbc7c25d8300cbfceb5f42ae829c5
颜恒富 <1315007322@qq.com>
2021-03-28 23:30:02 +0800

    闭包作用域

--- eventloop.js
+++ eventloop.js
@@ -7,20 +7,20 @@ setTimeout(() => {
   console.info(1);
 }, 1000);
 
-eventloop 事件循环
-js 单线程异步
+// eventloop 事件循环
+// js 单线程异步
 
-任务： 1. 同步任务， 2. 异步任务
+// 任务： 1. 同步任务， 2. 异步任务
 
-异步任务：
-1. 微任务  Promise, async/await, nexttick,
-2. 宏任务   setTimeout, setInterval requestAnimationFrame
+// 异步任务：
+// 1. 微任务  Promise, async/await, nexttick,
+// 2. 宏任务   setTimeout, setInterval requestAnimationFrame
 
-队列： 先进先出
-栈： 后进先出  先进后出
+// 队列： 先进先出
+// 栈： 后进先出  先进后出
 
-1. 先执行同步任务， 异步任务加入队列，等待 。
-2. 同步任务执行完成， 先执行异步任务队列中的微任务，
-3. 再去执行宏任务。
-4. 循环
+// 1. 先执行同步任务， 异步任务加入队列，等待 。
+// 2. 同步任务执行完成， 先执行异步任务队列中的微任务，
+// 3. 再去执行宏任务。
+// 4. 循环
 
