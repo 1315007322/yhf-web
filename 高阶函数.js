47ca7fd3eb99184ce2d1522038420861bd814990
jiangwei <760799669@qq.com>
2021-05-04 12:17:29 +0800

    hof

---
+++ 高阶函数.js
@@ -0,0 +1,27 @@
+// 判断一个变量是否是数组
+// instanceof
+const a = [1, 2, 3];
+
+const b = {};
+
+const c = new Date();
+
+// 高阶函数
+function type(o, b) {
+  return function (type) {
+    return function () {
+      return Object.prototype.toString.call(o) === `[object ${type}]`;
+    };
+  };
+}
+
+// 偏函数
+const checkType = type(a);
+
+console.info();
+console.info(type(a)("object"));
+console.info(checkType("Array"));
+
+type(a, b)("string");
+// 高阶组件
+connect(mapStatetopros, mapDispatchtoProps)(B);
